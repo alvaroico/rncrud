f33b4efe853d59ddfce1f4ef675962954d0ea3f9
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-03 19:34:03 -0300

    Context API

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, FlatList, Alert} from 'react-native';
 import {Avatar, Button, Icon, ListItem} from 'react-native-elements';
-import users from '../data/users';
-
-function confirmUserDeletion(user) {
-  Alert.alert('Excluir Usurário', 'Deseja excluir o usuário?', [
-    {
-      text: 'Sim',
-      onPress() {
-        console.warn('delete' + user.id);
-      },
-    },
-    {
-      text: 'Não',
-    },
-  ]);
-}
-
-function getActions(user, props) {
-  return (
-    <>
-      <Button
-        onPress={() => props.navigation.navigate('UserForm', user)}
-        type="clear"
-        icon={<Icon name="edit" size={25} color="orange" />}
-      />
-      <Button
-        onPress={() => confirmUserDeletion(user)}
-        type="clear"
-        icon={<Icon name="delete" size={25} color="red" />}
-      />
-    </>
-  );
-}
+import UserContext from '../context/UserContext';
 
 export default (props) => {
+  const {state} = useContext(UserContext);
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usurário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({item: user}) {
     return (
       <ListItem
@@ -46,7 +48,7 @@ export default (props) => {
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
-        <ListItem rightElement={getActions(user, props)} />
+        <ListItem rightElement={getActions(user)} />
       </ListItem>
     );
   }
@@ -55,7 +57,7 @@ export default (props) => {
     <View>
       <FlatList
         keyExtractor={(user) => user.id.toString()}
-        data={users}
+        data={state.users}
         renderItem={getUserItem}
       />
     </View>
