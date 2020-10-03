e6ee11f0fb2cbda636a6c7a140e0fb0f374c9e7b
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-03 17:46:33 -0300

    Lista de Usuários 01

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import {ListItem} from 'react-native-elements';
+import users from '../data/users';
 
 export default (props) => {
-  return <Text>UserList</Text>;
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        leftAvatar={{source: {uri: user.avatarUrl}}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
